c5cb0a8a6c9bdced1d105bddb0886b7c0d14be61
army <654193886@qq.com>
2020-11-05 20:02:12 +0800

    第一次提交测试。

---
+++ src/com/wfd360/Main.java
@@ -0,0 +1,9 @@
+package com.wfd360;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("-------xiangmu shang chuan ceshi---------");
+    }
+}
